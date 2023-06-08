fe8993d24e939efcac372f73275dadd6e08d5282
Wesley Simões <wsjdeveloper@gmail.com>
2023-06-08 14:18:11 -0300

    feat: add celebrate to validate Product routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 // Middleware para interceptar os erros da api
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
